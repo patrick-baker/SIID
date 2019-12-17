fb5dbed46d9179351f3c3086425f64a8f09d306e
Heather Walls <wallshkim@gmail.com>
2019-12-16 19:09:06 -0600

    Installs Sass

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
 
+// import './index.css';
+import '../sass/main.scss';
+
 import rootReducer from './redux/reducers'; // imports ./redux/reducers/index.js
 import rootSaga from './redux/sagas'; // imports ./redux/sagas/index.js
 
