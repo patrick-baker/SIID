4ccab2f2402d57e8c340f22decbe72a044fa45b0
Patrick Baker <bakerpj1992@gmail.com>
2019-12-16 18:47:09 -0600

    added ForgotPassword.js back in

---
+++ src/components/ForgotPassword/ForgotPassword.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+class ForgotPassword extends Component {
+    state = {
+        email: '',
+    }
+
+    // Event change listener for altering this.state.email that the user is sending a reset email to
+    handleChange = name => (event) => {
+        this.setState({
+            [name]: event.target.value,
+        });
+    };
+
+    sendEmail = (e) => {
+        e.preventDefault();
+        const { email } = this.state;
+        // dispatches to forgotPasswordSaga
+        this.props.dispatch({type: 'FORGOT_PASSWORD', payload: email})
+    };
+
+    render() {
+        const { email } = this.state;
+        const { messageFromServer, showNullError, showError } = this.props.forgotPassword
+
+        return (
+            <div>
+                <h4>Forgot Password Screen</h4>
+                <form className="profile-form" onSubmit={this.sendEmail}>
+                    <input
+                        id="email"
+                        label="email"
+                        value={email}
+                        onChange={this.handleChange('email')}
+                        placeholder="Email Address"
+                    />
+                    <button
+                    > Send Password Reset Email </button>
+                </form>
+                {showNullError && (
+                    <div>
+                        <p>The email address cannot be null.</p>
+                    </div>
+                )}
+                {showError && (
+                    <div>
+                        <p>
+                            That email address isn&apos;t recognized. Please try again or
+                            register for a new account.
+                         </p>
+                        <button
+                            onClick={() => {this.props.dispatch({type: 'SET_TO_REGISTER_MODE'})}}
+                        > Register </button>
+                    </div>
+                )}
+                {messageFromServer === 'recovery email sent' && (
+                    <div>
+                        <h3>Password Reset Email Successfully Sent!</h3>
+                    </div>
+                )}
+                <Link to="/home" > Back To Home </Link>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return state;
+  };
+  
+export default connect(mapStateToProps)(ForgotPassword);
