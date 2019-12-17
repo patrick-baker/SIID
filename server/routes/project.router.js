a60152731259170bef182dad7183162aa07fbab5
Ayriel Ash <aya37278@bethel.edu>
2019-12-17 17:03:51 -0600

    select projects for a given user id route

--- server/routes/project.router.js
+++ server/routes/project.router.js
@@ -6,7 +6,17 @@ const router = express.Router();
  * GET route template
  */
 router.get('/', (req, res) => {
+    const queryText=`SELECT "title","client", "description", "date_created" 
+    FROM "projects" WHERE "user_id"=$1`;
+    const queryValues=[req.user.id];
     
+    pool.query(queryText,queryValues)
+        .then(results=>{
+            res.send(results.rows);})
+            .catch((error)=>{
+            console.log('Error GET /project', error);
+            res.sendStatus(500);
+        })
 });
 
 /**
