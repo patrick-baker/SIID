cd16138768dc0f3a300c07591d0cc96121249cfe
David Searl <dhsearl@gmail.com>
2019-12-18 10:26:13 -0600

    initial commit on rules table

---
+++ src/components/RuleTable/RuleTable.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class ResetPassword extends Component {
+ 
+
+  render() {
+    
+    return (
+      <>
+      </>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return state;
+};
+
+export default connect(mapStateToProps)(ResetPassword);
