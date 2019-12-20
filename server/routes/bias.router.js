631c64516bbbf80aa20fb0339d074697f4d039ac
Thomas Dourgarian <Thomasdourgarian@gmail.com>
2019-12-20 09:36:49 -0600

    bias data saga and route

---
+++ server/routes/bias.router.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+
+router.get('/', async(req, res) => {
+    
+    try {
+        const queryText=`SELECT * FROM project_bias JOIN bias ON bias.id=project_bias.bias_id WHERE project_id=$1;`;
+        let biasData = await pool.query(queryText,[req.body.id]);
+        res.send(biasData.rows);
+    } catch(error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+module.exports = router;
---
+++ src/redux/sagas/biasSaga.js
@@ -0,0 +1,19 @@
+import { put, takeLatest, takeEvery } from 'redux-saga/effects';
+import axios from 'axios';
+
+// sends axios request to server to send update password email to user
+
+function* GET_BIAS_DATA(action) {
+    try {
+        let biasData = yield axios.get('/bias', { id: action.payload.id });
+        yield put({type:"SET_BIAS_DATA",payload:biasData.data});
+    } catch (error) {
+        console.log('error in FETCH_RULES saga', error);
+    }
+}
+
+function* RuleSaga() {
+    yield takeEvery('GET_BIAS_DATA', GET_BIAS_DATA);
+}
+
+export default RuleSaga;
