4ba9575b8881d7bddb7bbee0d1a1cd46873a0eaa
Heather Walls <wallshkim@gmail.com>
2019-12-30 12:26:27 -0600

    Bones for Projects page

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,21 +2,24 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 export class Projects extends Component {
+    componentDidMount() {
+        this.props.dispatch({ type: 'GET_PROJECT' });
+    }
+
     render() {
         return (
             <div>
+                <h1>projects</h1>
                 
+                <pre>{JSON.stringify(this.props.project, null, 2)}</pre>
             </div>
         )
     }
 }
 
-const mapStateToProps = (state) => ({
-    
-})
 
-const mapDispatchToProps = {
-    
+const mapStateToProps = (reduxState) => {
+    return reduxState
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Projects)
+export default connect(mapStateToProps)(Projects)
