d56542e67a574bc83dfb47678b522d102147df10
Heather Walls <wallshkim@gmail.com>
2019-12-30 16:25:24 -0600

    Creates project cards

--- src/components/Projects/ProjectItem.js
+++ src/components/Projects/ProjectItem.js
@@ -1,16 +1,20 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import moment from 'moment';
 
 class ProjectItem extends Component {
-
     render() {
+        // Create vars for project object and date
         const project = this.props.item;
+        const date = project.date_created;
         return (
+            // Create cards for each project
             <div>
-                <h1>{project.title}</h1>
-                <h2>{project.client}</h2>
-                <h3>{project.date_created}</h3>
-                <p>{project.description}</p>
+                <h1 className="project__title heading-tertiary">{project.title}</h1>
+                <h3 className="project__date body-bold">{moment(date).format("MMM Do, YYYY")}</h3>
+                <h2 className="project__client label">{project.client}</h2>
+                <p className="caption">{project.description}</p>
+                <button className="button__text">REPORT</button>
             </div>
         )
     }
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,17 +4,32 @@ import ProjectItem from './ProjectItem';
 
 class Projects extends Component {
     componentDidMount() {
+        // Get all projects for this user
         this.props.dispatch({ type: 'GET_PROJECT' });
+    }
+
+    handleClick = () => {
+        console.log('button clicked');
+        
     }
 
     render() {
         return (
             <div>
-                <h1>projects</h1>
+                {/* Display Create Project card */}
+                <div className="card__project">
+                    <div className="card__details">
+                        <h1 className="heading-secondary">CREATE NEW PROJECT</h1>
+                        <button onClick={this.handleClick}><i class="fas fa-plus"></i></button>
+                    </div>
+                </div>
+                {/* Display past project cards */}
                 {this.props.project.map(item => {
                     console.log('in projects map project is: ', item)
-                    return(
-                        <ProjectItem key={item.id} item={item}/>
+                    return (
+                        <div className="card__project">
+                            <ProjectItem key={item.id} item={item} />
+                        </div>
                     )
                 })}
                 <pre>{JSON.stringify(this.props.project, null, 2)}</pre>
