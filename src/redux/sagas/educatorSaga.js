e3adf8df5ed519bda94dd5edd946e460fc811c65
David Searl <dhsearl@gmail.com>
2019-12-31 14:13:21 -0600

    Educator deletion now prompts people

--- src/redux/sagas/educatorSaga.js
+++ src/redux/sagas/educatorSaga.js
@@ -3,47 +3,49 @@ import axios from 'axios';
 
 // sends axios request to server to send update password email to user
 function* addEducator(action) {
-  // console.log('action.payload of ForgotPasswordSaga:', action.payload);
-  try {
-    yield axios.post('/educator', action.payload);
-    yield put({type:"GET_EDUCATORS"});
-  } catch (error) {
-    console.log(error);
-  }
+    // console.log('action.payload of ForgotPasswordSaga:', action.payload);
+    try {
+        yield axios.post('/educator', action.payload);
+        yield put({ type: "GET_EDUCATORS" });
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 function* getEducators() {
-  try { 
-    let educators = yield axios.get('/educator');
-    yield put({type:"SET_EDUCATORS",payload: educators.data});
-  } catch(error) {
-    console.log(error);
-  }
+    try {
+        let educators = yield axios.get('/educator');
+        yield put({ type: "SET_EDUCATORS", payload: educators.data });
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 function* deleteEducator(action) {
     try {
-      yield axios.delete(`/educator/${action.payload.id}`);
-      yield put({type:"GET_EDUCATORS"});
-    }catch(error) {
-      console.log(error);
+        yield axios.delete(`/educator/${action.payload.id}`);
+        yield put({ type: "EDUCATOR_DEL_SUCCESS" })
+        yield put({ type: "GET_EDUCATORS" });
+    } catch (error) {
+        yield put({ type: "EDUCATOR_DEL_FAILURE" })
+        console.log('Error deleting educator was', error);
     }
 }
 
 function* updateEducator(action) {
     try {
-      yield axios.put(`/educator/`,action.payload);
-      yield put({type:"GET_EDUCATORS"});
-    }catch(error) {
-      console.log(error);
+        yield axios.put(`/educator/`, action.payload);
+        yield put({ type: "GET_EDUCATORS" });
+    } catch (error) {
+        console.log(error);
     }
 }
 
 function* EducatorSaga() {
-  yield takeLatest('ADD_EDUCATOR', addEducator);
-  yield takeLatest('GET_EDUCATORS',getEducators)
-  yield takeLatest('DELETE_EDUCATOR',deleteEducator)
-  yield takeLatest('UPDATE_EDUCATOR',updateEducator)
+    yield takeLatest('ADD_EDUCATOR', addEducator);
+    yield takeLatest('GET_EDUCATORS', getEducators)
+    yield takeLatest('DELETE_EDUCATOR', deleteEducator)
+    yield takeLatest('UPDATE_EDUCATOR', updateEducator)
 }
 
 export default EducatorSaga;
