1143073b1b4b49212bcd1fe7e6d16884cccef32c
David Searl <dhsearl@gmail.com>
2020-01-03 13:59:57 -0600

    removed more console logs and extra lines

--- src/components/Projects/ProjectItem.js
+++ src/components/Projects/ProjectItem.js
@@ -10,10 +10,6 @@ class ProjectItem extends Component {
         this.props.history.push(`/report/${this.props.item.id}`);
     }
 
-    // handleDeleteProject = (id) => {
-    //     this.props.dispatch({type: 'REMOVE_PROJECT', payload: id})
-    // }
-
     render() {
         // Create vars for project object and date
         const project = this.props.item;
