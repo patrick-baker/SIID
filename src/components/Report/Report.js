fb1cddfa2002b379c3bbbce8cf14e9fd6a4399d6
David Searl <dhsearl@gmail.com>
2020-01-03 15:32:06 -0600

    fixed the layout of the json.stringinfy

--- src/components/Report/Report.js
+++ src/components/Report/Report.js
@@ -21,11 +21,11 @@ class Report extends Component {
                     {JSON.stringify(this.props.reportReducer, null, 2)}
                     Flag Reducer:
                 {JSON.stringify(this.props.flagReducer, null, 2)}
-                </pre>
+                
                 Bias Reducer back from database: {this.props.biasDataReducer.status ? "YES" : "NO"}
-                {this.props.biasDataReducer.status &&
-                    <pre>{JSON.stringify(this.props.biasDataReducer.data, null, 2)}</pre>}
-
+                {this.props.biasDataReducer.status &&<>
+                    {JSON.stringify(this.props.biasDataReducer.data, null, 2)}</>}
+</pre>
             </div>
         )
     }
