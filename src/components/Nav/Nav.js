b25c0b6f3b100525a46d09d631d23023afbfb03a
Patrick Baker <bakerpj1992@gmail.com>
2020-01-07 14:36:16 -0600

    app layout no longer flex, uses fixed nav and absolute positioned content

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,22 +6,24 @@ import LogOutButton from '../LogOutButton/LogOutButton';
 const Nav = (props) => (
   <div className="navigation">
     <div className="navigation__background">
-      
+
       <div className="navigation__list">
-          <div className="navigation__logo" >
+        <div className="navigation__logo" >
           <h1 className="heading-primary" >SIID</h1>
-          </div>
+        </div>
         <div className="navigation__item" >
           <Link className="navigation__link" to="/home">
             <i class="fas fa-th fa-2x navigation__icon"></i>{props.user.id ? 'Projects' : 'Login / Register'}
           </Link>
         </div>
 
-        <div className="navigation__item">
-          <Link className="navigation__link" to="/rules">
-            <i class="fas fa-clipboard-list fa-2x navigation__icon" ></i>Rules
-        </Link>
-        </div>
+        {props.user.admin === true &&
+          <div className="navigation__item">
+            <Link className="navigation__link" to="/rules">
+              <i class="fas fa-clipboard-list fa-2x navigation__icon" ></i>Rules
+            </Link>
+          </div>
+        }
 
         <div className="navigation__item">
           {props.user.id &&
@@ -39,35 +41,7 @@ const Nav = (props) => (
           }
         </div>
       </div>
-      {/* <ul className="navigation__list">
-        <div className="navigation__item" >
-          <Link className="navigation__link" to="/home">
-            <i class="fas fa-th fa-2x navigation__icon"></i>{props.user.id ? 'Projects' : 'Login / Register'}
-          </Link>
-        </div>
 
-        <div className="navigation__item">
-          <Link className="navigation__link" to="/rules">
-            <i class="fas fa-clipboard-list fa-2x navigation__icon" ></i>Rules
-        </Link>
-        </div>
-
-        <div className="navigation__item">
-          {props.user.id &&
-            <Link className="navigation__link" to="/educators">
-              <i class="fas fa-graduation-cap fa-2x navigation__icon"></i>Educators
-            </Link>
-          }
-        </div>
-
-        <div className="navigation__item">
-          {props.user.id &&
-            <Link className="navigation__link" onClick={() => props.dispatch({ type: 'LOGOUT' })}>
-              <i class="fas fa-sign-out-alt fa-2x navigation__icon"></i>Log out
-            </Link>
-          }
-        </div>
-      </ul> */}
     </div>
   </div >
 );
