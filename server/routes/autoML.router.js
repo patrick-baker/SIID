e43b66d173b20085ae735205b2eba4836a04325d
Ayriel Ash <aya37278@bethel.edu>
2020-01-07 12:01:21 -0600

    inital data display

--- server/routes/autoML.router.js
+++ server/routes/autoML.router.js
@@ -27,7 +27,8 @@ router.post("/", async (req, res) => {
         for (biasKey of Object.keys(data)) {
             if (data[biasKey].count > 0) {
                 let biasId = await pool.query(`SELECT id FROM bias WHERE type=$1;`,[biasKey])
-                await pool.query(`INSERT INTO project_bias(project_id,bias_id,bias_count) VALUES($1,$2,$3)`,[req.body.project_id,biasId.rows[0].id,data[biasKey].count])
+                console.log('in autoML router the biasID is:', biasId.rows[0].id, 'bias count is:', data[biasKey].count, 'project_id',req.body.project_id)
+                await pool.query(`INSERT INTO "project_bias" (project_id,bias_id,bias_count) VALUES($1,$2,$3)`,[req.body.project_id,biasId.rows[0].id,data[biasKey].count])
             }
         }
         await pool.query('UPDATE project SET analyzed=$1 WHERE id=$2;',[true,req.body.project_id])
