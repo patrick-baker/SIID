62e3d5d814ef632a3a0ef7fdf6b17987cdd37a65
Ayriel Ash <aya37278@bethel.edu>
2020-01-11 16:33:32 -0600

    change icon, start tone layout, bubble interactivity begins

--- src/components/Report/Tone/Tone.js
+++ src/components/Report/Tone/Tone.js
@@ -6,35 +6,42 @@ class Tone extends Component {
     render() {
         return (
             <div className="report__tone">
+                <div className="report__targetInfo__left">
                 <h2>Target Audience:</h2>
-                <p className="report__header3">Age:
-                    <span className="report__text">{this.props.reportReducer.target_audience_age}</span>
-                    Gender:
-                    <span className="report__text">{this.props.reportReducer.target_audience_gender}</span>
-                    Region:
-                    <span className="report__text">    {this.props.reportReducer.target_audience_region}   </span>
-                </p>
-                <p>{this.props.reportReducer.talent_demographic}</p>
-                <p>{this.props.reportReducer.target_audience_ethnicity}</p>
-  
-                <p>{this.props.reportReducer.target_audience_interests}</p>
-                <p>{this.props.reportReducer.target_audience_language}</p>
-                <p>{this.props.reportReducer.target_audience_race}</p>
+                <table>
+                <tr>
+                    <td className="label">Age:</td><td>{this.props.reportReducer.target_audience_age}</td>
+                    <td className="label">Gender:</td><td>{this.props.reportReducer.target_audience_gender}</td>                    
+                    <td className="label">Region:</td><td>{this.props.reportReducer.target_audience_region}</td>
+                    <td className="label">Language:</td><td>{this.props.reportReducer.target_audience_language}</td>
+                </tr>
+                <tr>
+                    <td className="label">Race:</td><td>{this.props.reportReducer.target_audience_race}</td>
+                    <td className="label">Ethnicity:</td><td>{this.props.reportReducer.target_audience_ethnicity}</td>                    
+                    <td className="label">Interests:</td><td>{this.props.reportReducer.target_audience_interests}</td>
+                    <td className="label">Talent Demographic:</td><td>{this.props.reportReducer.talent_demographic}</td>
+                </tr>
+                </table>
+                </div>
+              
      
-
+                <div className="report__targetInfo__middle">
                 <h2>Tone</h2>
-                <ul>
+                <ul className="report__targetInfo__list">
                     {this.props.reportReducer.formal ? <li>Formal</li> : <li>Informal</li>}
                     {this.props.reportReducer.tone.map(type => {
                         return <li>{type}</li>
                     })}
                 </ul>
+                </div>
+                <div className="report__targetInfo__right">
                 <h2>Literary Techniques</h2>
-                <ul>
+                <ul className="report__targetInfo__list">
                     {this.props.reportReducer.literaryTechniques.map(lit => {
                         return <li>{lit}</li>
                     })}
                 </ul>
+                </div>
             </div>
         );
     }
